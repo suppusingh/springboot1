c39432b1be8160d61fffd94aad6f887f5e54a5d6
DESKTOP-N58ELU8\supri <supriya.k.singh@outlook.com>
2024-08-01 15:54:52 -0500

    spring practice

---
+++ suppu.spring.info/suppu.spring.info/src/main/java/suppu/spring/info/suppu/spring/info/SpringbootpracticeApplication.java
@@ -0,0 +1,13 @@
+package suppu.spring.info.suppu.spring.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpracticeApplication.class, args);
+	}
+
+}
---
+++ suppu.spring.info/suppu.spring.info/src/test/java/suppu/spring/info/suppu/spring/info/SpringbootpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package suppu.spring.info.suppu.spring.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
